3af0096346dc6362a0a72b387a315d2c9d553b3e
FrantisekSefcik <frantisek.sefcik@gmail.com>
2020-05-06 12:11:54 +0200

    ADD: docstring to utils functions

--- interpretable-nn/helpers/utils.py
+++ interpretable-nn/helpers/utils.py
@@ -2,25 +2,49 @@ import numpy as np
 
 
 def get_mask_for_gray(segmentation, threshold=0, exact=None):
+    """
+    Get mask of tumor as gray image
+
+    :param segmentation: segmentation image
+    :param threshold: threshold for tumor region
+    :param exact: True if mask is count as equation of threshold
+    :return: mask of tumor with shape (height, width, 1)
+    """
     if exact:
         return segmentation == exact
     else:
         return segmentation > threshold
 
 
-def get_mask_of_seg_rgb(segmentation, threshold=0, nchannels=3, exact=None):
+def get_mask_of_seg_rgb(segmentation, threshold=0, exact=False):
+    """
+    Get mask of tumor for rgb image
+
+    :param segmentation: segmentation image
+    :param threshold: threshold for tumor region
+    :param exact: True if mask is count as equation of threshold
+    :return: mask of tumor with shape (height, width, 3)
+    """
     height, width = segmentation.shape[0], segmentation.shape[1]
-    new_mask = np.zeros((height, width, nchannels), dtype=bool)
+    new_mask = np.zeros((height, width, 3), dtype=bool)
     if exact:
-        new_seg = (segmentation == exact)[:, :, 0]
+        new_seg = (segmentation == threshold)[:, :, 0]
     else:
         new_seg = (segmentation > threshold)[:, :, 0]
-    for ch in range(nchannels):
+    for ch in range(3):
         new_mask[:, :, ch] = new_seg
     return new_mask
 
 
 def get_mask_of_brain(image, segmentation=None):
+    """
+    Get mask of brain from original image as gray image
+    mask = brain mask - tumor mask
+
+    :param image: original image
+    :param segmentation: tumor segmentation image
+    :return: mask of brain without tumor region with shape (height, width, 1)
+    """
     if segmentation is not None and image.shape != segmentation.shape:
         raise Exception('Not equal shape of image and segmentation')
     if len(image.shape) == 3:
@@ -30,6 +54,14 @@ def get_mask_of_brain(image, segmentation=None):
 
 
 def get_mask_of_brain_rgb(image, segmentation=None):
+    """
+    Get mask of brain from original image as rgb image
+    mask = brain mask - tumor mask
+
+    :param image: original image
+    :param segmentation: tumor segmentation image
+    :return: mask of brain without tumor region with shape (height, width, 3)
+    """
     if segmentation is not None and image.shape != segmentation.shape:
         raise Exception('Not equal shape of image and segmentation')
     if segmentation is not None:
@@ -41,10 +73,20 @@ def get_mask_of_brain_rgb(image, segmentation=None):
 
 
 def sum_image_channels(image):
+    """
+    Sum values cross channels in image
+    :param image: original image
+    :return: ndarray with shape (height, weight, 1)
+    """
     return image.sum(axis=np.argmax(np.asarray(image.shape) == 3))
 
 
 def any_image_channels(image):
+    """
+    Any value cross channels of image
+    :param image: original image
+    :return: ndarray with shape (height, weight, 1)
+    """
     return image.any(axis=np.argmax(np.asarray(image.shape) == 3))
 
 
